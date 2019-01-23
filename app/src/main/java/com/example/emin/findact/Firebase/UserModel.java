3519bbb290ebd4ddd3902d1f5ef27e73aa86d145
enescakmak <1306140051@ogr.iu.edu.tr>
2019-01-23 21:18:25 +0300

    Uygulama kayıtlı isen seni sürekli olarak GetUserDetailActivity atıcak.

--- app/src/main/java/com/example/emin/findact/Firebase/UserModel.java
+++ app/src/main/java/com/example/emin/findact/Firebase/UserModel.java
@@ -2,15 +2,20 @@ package com.example.emin.findact.Firebase;
 
 public class UserModel {
 
-    String name, surname, city, birthday;
+    String name, surname, city, birthday, image;
     ActivitiesModel activies;
 
-    public UserModel(String name, String surname, String city, String birthday, ActivitiesModel activitiesModel) {
+    public UserModel(String name, String surname, String city, String birthday,String image, ActivitiesModel activitiesModel) {
         this.name = name;
         this.surname = surname;
         this.city = city;
         this.birthday = birthday;
+        this.image = image;
         this.activies = activitiesModel;
+    }
+
+    public String getImage() {
+        return image;
     }
 
     public String getName() {
